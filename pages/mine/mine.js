dd188af5fb82e9af23cefb172e127cdabf700498
kaleido <2087331365@qq.com>
2024-02-19 00:34:30 +0800

    添加mine页面表格逻辑

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,22 +1,41 @@
 // pages/mine/mine.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    control: [
-      { date: '2023.1.1', time: '12:00:00' },
-      { date: '2023.1.2', time: '12:00:00' },
-      { date: '2023.1.3', time: '12:00:00' }
-    ]
+    controlData: [
+      { date: '2021.2.2', time: '12:00:00' },
+      { date: '2024.1.1', time: '12:00:00' },
+      { date: '2022.1.2', time: '12:00:00' },
+      { date: '2023.1.3', time: '12:00:00' },
+      { date: '2022.1.4', time: '12:00:00' }
+    ],
+    controlYear: [],
+    controlList: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    let controlData = this.data.controlData;
+    let yearList = [];
+    let dataByYear = {};
+    for (let item of controlData) {
+      let year = item.date.split(".")[0];
+      if (!dataByYear[year]) {
+        yearList.push(year);
+        dataByYear[year] = [];
+      }
+      dataByYear[year].push(item);
+    }
 
+    yearList.sort(function(a, b) {
+      return b - a;
+    }) 
+
+    this.setData({
+      controlYear: yearList,
+      controlList: dataByYear
+    });
   },
 
   /**
